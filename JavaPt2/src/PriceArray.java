221e1b7b4cf5bad1c001e72bfbaf6f35924d5543
Stephanie Scott <stephanie.scott.1719@gmail.com>
2021-10-18 12:38:37 -0500

    update arrays

---
+++ JavaPt2/src/PriceArray.java
@@ -0,0 +1,6 @@
+public class PriceArray {
+    public static void main(String[] args) {
+        int[] myArray = { 4, 6, 7, 10, 3, 9, 8 };
+        System.out.println(myArray[myArray.length - 1]);
+    }
+}
