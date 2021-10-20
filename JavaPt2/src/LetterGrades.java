eaee6d401ca5220ab529a896c561235680b0d8e3
Stephanie Scott <stephanie.scott.1719@gmail.com>
2021-10-20 13:45:18 -0500

    10/20 updates

---
+++ JavaPt2/src/LetterGrades.java
@@ -0,0 +1,9 @@
+public class LetterGrades {
+    public static void main(String[] args) {
+        char[] letterGrades = { 'A', 'B', 'C' };
+
+        for (int i = 0; i < letterGrades.length; i++) {
+            System.out.println(letterGrades[i]);
+        }
+    }
+}
